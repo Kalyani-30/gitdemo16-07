e26dfe009748caf08f1a4f70e715e78910e2190b
KALYANI <gandukalyani1@gmail.com>
2025-07-16 12:21:44 +0530

    git commit

---
+++ src/main/java/com/java/neoteric/GitPracties.java
@@ -0,0 +1,13 @@
+package com.java.neoteric;
+
+public class GitPracties {
+    int a= 10;
+    int b = 20;
+    int c = a+b;
+    public static void main(String[] args) {
+        GitPracties gitPracties= new GitPracties();
+
+        System.out.println( gitPracties.c);
+
+    }
+}
